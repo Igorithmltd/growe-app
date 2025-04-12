03b389ad1cadb98b98f60130a4e6d7d6e5b118a2
Olubodun <bolowys33@gmail.com>
2025-04-12 13:16:12 +0100

    create loader and spinner component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,5 @@ export { StyledText } from "./text";
 export { StyledButton } from "./button";
 export { StyledField } from "./input";
 export { PasswordInput } from "./password-input";
-export {StyledPinInput} from "./pin-input";
+export { StyledPinInput } from "./pin-input";
+export { Loader, Spinner } from "./loader";
