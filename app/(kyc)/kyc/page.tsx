9d1e803a0358dac00174b22cfbdae4e9d1ae1d6e
Olubodun <bolowys33@gmail.com>
2025-04-18 19:22:17 +0100

    Remove Suspense and Loader from KYC page for simplified rendering

--- app/(kyc)/kyc/page.tsx
+++ app/(kyc)/kyc/page.tsx
@@ -1,18 +1,12 @@
-import { Loader } from "@/src/components";
 import KycLayout from "@/src/layouts/kyc";
 import { Metadata } from "next";
-import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Growe | KYC",
 };
 
 const KycPage = () => {
-  return (
-    <Suspense fallback={<Loader />}>
-      <KycLayout />
-    </Suspense>
-  );
+  return <KycLayout />;
 };
 
 export default KycPage;
