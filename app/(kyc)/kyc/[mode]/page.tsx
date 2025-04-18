df38ed4e2dc5f9be231b850e7519c49a0b235577
Olubodun <bolowys33@gmail.com>
2025-04-18 19:26:28 +0100

    Remove Suspense and Loader from VerificationPage for simplified rendering

--- app/(kyc)/kyc/[mode]/page.tsx
+++ app/(kyc)/kyc/[mode]/page.tsx
@@ -1,8 +1,6 @@
-import { Loader } from "@/src/components";
 import BVNLayout from "@/src/layouts/kyc/bvn";
 import NINLayout from "@/src/layouts/kyc/nin";
 import { Metadata } from "next";
-import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Growe | KYC",
@@ -13,14 +11,14 @@ interface PageParams {
 }
 
 interface PageProps {
-  params: Promise<PageParams>;
+  params: Promise<PageParams>; 
 }
 
 const VerificationPage = async ({ params }: PageProps) => {
   const { mode } = await params;
   const isBvn = mode === "bvn";
 
-  return <Suspense fallback={<Loader />}>{isBvn ? <BVNLayout /> : <NINLayout />}</Suspense>;
+  return isBvn ? <BVNLayout /> : <NINLayout />;
 };
 
 export default VerificationPage;
