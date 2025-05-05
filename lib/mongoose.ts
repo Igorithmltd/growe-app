90f42c3f46dc64bdb560976331ed431bd32206c8
Olubodun <bolowys33@gmail.com>
2025-05-05 08:27:39 +0100

    fix: update mongoose global variable declaration for type safety

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -2,7 +2,10 @@ import mongoose, { Connection } from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached = global.mongoose;
+let cached = global.mongoose as {
+  conn: Connection | null;
+  promise: Promise<Connection> | null;
+};
 
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
