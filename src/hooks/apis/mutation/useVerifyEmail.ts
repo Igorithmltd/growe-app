922662a93bdeed99448a009745ae49b3b556de1a
Olubodun <bolowys33@gmail.com>
2025-05-06 18:24:05 +0100

    refactor: remove unused showToast import in useVerifyEmail hook

--- src/hooks/apis/mutation/useVerifyEmail.ts
+++ src/hooks/apis/mutation/useVerifyEmail.ts
@@ -5,11 +5,8 @@ import { useMutation } from "@tanstack/react-query";
 import { handleError } from "@/src/utils/helpers";
 import { useFetcher } from "../../useFetcher";
 import { VerifyFormValue } from "@/src/schema/auth.schema";
-import useShowToast from "../../useShowToast";
 
 export const useVerifyEmail = () => {
-  const showToast = useShowToast();
-
   const verifyEmail = async (data: VerifyFormValue): Promise<AuthResponseData> => {
     const response = await useFetcher({
       url: "/auth/verify-email",
