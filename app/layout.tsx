ccf6bf6b98c8725df3bd87a85bc55fcd3561db82
Olubodun <bolowys33@gmail.com>
2025-05-07 10:51:43 +0100

    feat: wrap children with ModalProvider in RootLayout for modal state management

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Providers } from "./providers";
 import "./globals.css";
 import TanStackProviders from "./tankstack-provider";
 import { Toaster } from "@/src/components/ui/toaster";
+import { ModalProvider } from "@/src/contexts/ModalContext";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -9,7 +10,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <Providers>
           <Toaster />
-          <TanStackProviders>{children}</TanStackProviders>
+          <TanStackProviders>
+            <ModalProvider>{children}</ModalProvider>
+          </TanStackProviders>
         </Providers>
       </body>
     </html>
