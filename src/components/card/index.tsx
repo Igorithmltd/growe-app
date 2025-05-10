bc8fa903ceb583cb39f569d5a13a2fe4ae39cd19
Olubodun <bolowys33@gmail.com>
2025-05-10 12:43:45 +0100

    feat: add ReusableCard component for consistent card UI with customizable border radius

---
+++ src/components/card/index.tsx
@@ -0,0 +1,16 @@
+import { Box, BoxProps } from "@chakra-ui/react";
+
+interface ReusableCardProps extends BoxProps {
+  borderRadius?: string | number;
+  children?: React.ReactNode;
+}
+
+const ReusableCard: React.FC<ReusableCardProps> = ({ borderRadius = "md", children, ...props }) => {
+  return (
+    <Box borderRadius={borderRadius} boxShadow="md" p={4} bg="white" {...props}>
+      {children}
+    </Box>
+  );
+};
+
+export default ReusableCard;
