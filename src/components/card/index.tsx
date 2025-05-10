f593ee89c9464175ea4e7a1523c801aac9519902
Olubodun <bolowys33@gmail.com>
2025-05-10 17:26:02 +0100

    fix: correct export name of ReusableCard component to ReuseableCard for consistency

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -5,12 +5,14 @@ interface ReusableCardProps extends BoxProps {
   children?: React.ReactNode;
 }
 
-const ReusableCard: React.FC<ReusableCardProps> = ({ borderRadius = "md", children, ...props }) => {
+export const ReuseableCard: React.FC<ReusableCardProps> = ({
+  borderRadius = "md",
+  children,
+  ...props
+}) => {
   return (
     <Box borderRadius={borderRadius} boxShadow="md" p={4} bg="white" {...props}>
       {children}
     </Box>
   );
 };
-
-export default ReusableCard;
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,4 +4,5 @@ export { StyledField } from "./input";
 export { PasswordInput } from "./password-input";
 export { StyledPinInput } from "./pin-input";
 export { Modal } from "./modal";
+export { ReuseableCard } from "./card";
 export { Loader, Spinner } from "./loader";
