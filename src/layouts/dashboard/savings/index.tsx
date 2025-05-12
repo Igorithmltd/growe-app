edf560cecd7797554b7e348673ead2f0117eb6eb
Olubodun <bolowys33@gmail.com>
2025-05-12 14:37:11 +0100

    feat: create PersonalSavings component to display savings details and actions

--- src/layouts/dashboard/savings/index.tsx
+++ src/layouts/dashboard/savings/index.tsx
@@ -1,64 +1,17 @@
 "use client";
 
-import { StyledButton, StyledText } from "@/src/components";
-import { Box, VStack } from "@chakra-ui/react";
-import { ActiveSavingsCard, EmptyCard, SavingsCard } from "../cards";
-import { FaPlus } from "react-icons/fa6";
+import { StyledText } from "@/src/components";
+import { Box } from "@chakra-ui/react";
+import PersonalSavings from "./personal";
 
 const SavingsLayout = () => {
-  const isEmpty = !true;
-
   return (
     <Box p={2}>
       <StyledText fontSize={{ base: "xl", md: "2xl" }} fontWeight="medium" color="secondary">
         My Savings
       </StyledText>
 
-      <VStack align="stretch" spaceY={5} mt={6}>
-        <SavingsCard
-          title="Total Personal Savings"
-          amount="0.00"
-          amountColor="secondary"
-          interest="0%"
-          bg="white"
-          color="bfgrey"
-          buttonBg="#F8FBEB"
-          boxShadow="sm"
-          buttonText={
-            <>
-              <FaPlus size="18px" /> Quick Save
-            </>
-          }
-          buttonAction={() => {}}
-        />
-
-        <StyledButton type="button" alignSelf={{ lg: "start" }}>
-          <FaPlus size="18px" /> Create New Savings Goal
-        </StyledButton>
-      </VStack>
-      <Box mt={12}>
-        <StyledText
-          fontSize={{ base: "md", md: "lg", lg: "xl" }}
-          fontWeight="medium"
-          color="secondary"
-        >
-          Active Savings
-        </StyledText>
-
-        <VStack align="stretch" spaceY={4} mt={6}>
-          {isEmpty ? (
-            <EmptyCard title="You Don’t Have Any Active Savings Yet!" />
-          ) : (
-            <VStack align="stretch" spaceY={4} mt={6}>
-              <ActiveSavingsCard amount="800,000" name="Rent" plan="6 months" value={90} />
-              <ActiveSavingsCard amount="800,000" name="Rent" plan="6 months" value={10} />
-              <ActiveSavingsCard amount="800,000" name="Rent" plan="6 months" value={25} />
-              <ActiveSavingsCard amount="800,000" name="Rent" plan="6 months" value={50} />
-              <ActiveSavingsCard amount="800,000" name="Rent" plan="6 months" value={75} />
-            </VStack>
-          )}
-        </VStack>
-      </Box>
+      <PersonalSavings />
     </Box>
   );
 };
