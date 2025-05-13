bfa626febe8fd655fe6876a5e4ff1d696a1100da
Olubodun <bolowys33@gmail.com>
2025-05-13 19:50:06 +0100

    feat: implement SavingGoalLayout component with form handling and validation

--- src/schema/savings.schema.ts
+++ src/schema/savings.schema.ts
@@ -1,7 +1,7 @@
 import * as Yup from "yup";
 
 export const quickSavingSchema = Yup.object().shape({
-  amount: Yup.number().required("Amount is required").min(1000),
+  amount: Yup.number().required("Amount is required").min(1000, "Minimum ammount is ₦1000"),
 });
 
 export type QuickSavingValues = Yup.InferType<typeof quickSavingSchema>;
