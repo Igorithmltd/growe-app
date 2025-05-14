8d9d25b99e0e3bb995c05e5a47cc4eca94ace0db
Olubodun <bolowys33@gmail.com>
2025-05-14 08:32:37 +0100

    fix: remove isGrid prop from SelectButtonGroup in SavingGoalLayout

--- src/layouts/dashboard/savings/personal/savings-goal/index.tsx
+++ src/layouts/dashboard/savings/personal/savings-goal/index.tsx
@@ -126,7 +126,6 @@ const SavingGoalLayout = () => {
             />
 
             <SelectButtonGroup
-              isGrid
               label="For"
               labelColor="secondary"
               options={["6 months", "9 months", "1 year", "Let me choose"]}
