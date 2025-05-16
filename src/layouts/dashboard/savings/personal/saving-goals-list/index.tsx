6b98774642a40e8e5b29285002d904991618ea7c
Olubodun <bolowys33@gmail.com>
2025-05-16 16:31:00 +0100

    feat: add SavingGoalList component for displaying savings goals

---
+++ src/layouts/dashboard/savings/personal/saving-goals-list/index.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react";
+
+const SavingGoalList = () => {
+  return <Box px={6} py={{ base: 5, lg: 10 }} w={{ lg: "65%" }} mx="auto">
+    
+  </Box>;
+};
+
+export default SavingGoalList;
