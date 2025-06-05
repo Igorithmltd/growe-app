a80bd760e7b9b65c9968a6c29682f658288a1a5a
Olubodun <bolowys33@gmail.com>
2025-06-05 16:19:52 +0100

    Add SavingsGroupPage component to display investment terms and conditions

---
+++ app/(dashboard)/investments/join-group/[id]/terms/page.tsx
@@ -0,0 +1,12 @@
+import InvestmentGroupTerms from "@/src/layouts/dashboard/investments/group/joined-group copy/terms-conditions";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Growe | Investment Terms & Conditions",
+};
+
+const SavingsGroupPage = () => {
+  return <InvestmentGroupTerms />;
+};
+
+export default SavingsGroupPage;
