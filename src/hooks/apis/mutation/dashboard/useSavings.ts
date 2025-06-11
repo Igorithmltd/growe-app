6b9ff0ee5b434dbf9c0a709b73a4eff2286b4700
Olubodun <bolowys33@gmail.com>
2025-06-11 10:21:37 +0100

    Add createSavingGroup mutation and update useSavings hook for group creation

--- src/hooks/apis/mutation/dashboard/useSavings.ts
+++ src/hooks/apis/mutation/dashboard/useSavings.ts
@@ -5,7 +5,7 @@ import { useMutation } from "@tanstack/react-query";
 import { handleError } from "@/src/utils/helpers";
 import { useFetcher } from "../../../useFetcher";
 import useShowToast from "../../../useShowToast";
-import { SavingsGoalValues } from "@/src/schema/savings.schema";
+import { CreateGroupValues, SavingsGoalValues } from "@/src/schema/savings.schema";
 
 export const useSavings = () => {
   const showToast = useShowToast();
@@ -23,7 +23,20 @@ export const useSavings = () => {
     return response.data;
   };
 
-  const { mutate: createSavingsGoal, isPending: isCreating } = useMutation<
+  const createGroup = async (data: CreateGroupValues): Promise<AuthResponseData> => {
+    const response = await useFetcher({
+      url: "/auth/forgot-password",
+      requestType: "POST",
+      body: data,
+      useBaseUrl: true,
+    });
+
+    if (response.error) handleError(response.error);
+
+    return response.data;
+  };
+
+  const { mutate: createSavingsGoal, isPending: isCreatingGoal } = useMutation<
     AuthResponseData,
     AxiosError<ErrorResponseData>,
     SavingsGoalValues
@@ -60,5 +73,42 @@ export const useSavings = () => {
     },
   });
 
-  return { createSavingsGoal, isCreating };
+  const { mutate: createSavingGroup, isPending: isCreatingGroup } = useMutation<
+    AuthResponseData,
+    AxiosError<ErrorResponseData>,
+    CreateGroupValues
+  >({
+    mutationFn: createGoal,
+    onError: (error) => {
+      if (error.response) {
+        const errorData = error.response.data;
+        showToast({
+          title: "Error",
+          description: errorData.message || "Something went wrong on the server",
+          status: "error",
+        });
+      } else if (error.request) {
+        showToast({
+          title: "Network Error",
+          description: "No response received from the server, try again",
+          status: "warning",
+        });
+      } else {
+        showToast({
+          title: "Error",
+          description: error.message || "An unknown error occurred",
+          status: "error",
+        });
+      }
+    },
+    onSuccess: () => {
+      showToast({
+        title: "Success",
+        description: "Savings Group created successfully",
+        status: "success",
+      });
+    },
+  });
+
+  return { createSavingsGoal, isCreatingGoal, createSavingGroup, isCreatingGroup };
 };
