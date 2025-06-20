bb7c62999a68913e40eb5842816529bfbf075689
Olubodun <bolowys33@gmail.com>
2025-06-20 09:46:43 +0100

    Add Finance Notes page and layout with navigation support

---
+++ src/layouts/dashboard/finance-notes/index.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Box, VStack } from "@chakra-ui/react";
+//
+import { StyledText } from "@/src/components";
+// import { ROUTES } from "@/src/utils/constants";
+import { useRouter } from "next/navigation";
+import { BackIcon } from "@/public/svgs";
+
+const FinanceNotesLayout = () => {
+  const router = useRouter();
+
+  const handleBack = () => {
+    router.back();
+  };
+
+  return (
+    <Box px={{ base: 3, md: 6 }} py={{ base: 5, lg: 10 }} w={{ lg: "65%" }} mx="auto">
+      <Box display="flex" gap={4} alignItems="center" mt={{ base: 6, lg: "unset" }}>
+        <Box cursor="pointer" onClick={handleBack}>
+          <BackIcon />
+        </Box>
+
+        <StyledText fontSize={{ base: "xl", md: "2xl" }} fontWeight="medium" color="secondary">
+          Finance Notes
+        </StyledText>
+      </Box>
+
+      <VStack align="stretch" spaceY={8} mt={14}></VStack>
+    </Box>
+  );
+};
+
+export default FinanceNotesLayout;
