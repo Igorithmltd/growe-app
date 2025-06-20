48ffa28c95ce814945c2693ee1cedb5a467b8e1f
Olubodun <bolowys33@gmail.com>
2025-06-20 10:01:30 +0100

    Enhance Finance Notes layout with additional components and styling

--- src/layouts/dashboard/finance-notes/index.tsx
+++ src/layouts/dashboard/finance-notes/index.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { Box, VStack } from "@chakra-ui/react";
+import { Box, HStack, VStack } from "@chakra-ui/react";
 //
-import { StyledText } from "@/src/components";
+import { StyledButton, StyledText } from "@/src/components";
 // import { ROUTES } from "@/src/utils/constants";
 import { useRouter } from "next/navigation";
 import { BackIcon } from "@/public/svgs";
@@ -15,7 +15,16 @@ const FinanceNotesLayout = () => {
   };
 
   return (
-    <Box px={{ base: 3, md: 6 }} py={{ base: 5, lg: 10 }} w={{ lg: "65%" }} mx="auto">
+    <Box
+      px={{ base: 3, md: 6 }}
+      py={{ base: 5, lg: 10 }}
+      w={{ lg: "65%" }}
+      mx="auto"
+      bg="red"
+      minH="80vh"
+      position="relative"
+      pb="80px"
+    >
       <Box display="flex" gap={4} alignItems="center" mt={{ base: 6, lg: "unset" }}>
         <Box cursor="pointer" onClick={handleBack}>
           <BackIcon />
@@ -27,6 +36,21 @@ const FinanceNotesLayout = () => {
       </Box>
 
       <VStack align="stretch" spaceY={8} mt={14}></VStack>
+      <Box
+        position="absolute"
+        bottom={4}
+        left={0}
+        right={0}
+        display="flex"
+        justifyContent="center"
+        px={4}
+      >
+        <Box w="100%">
+          <StyledButton type="button" w="100%">
+            Create Notes
+          </StyledButton>
+        </Box>
+      </Box>
     </Box>
   );
 };
