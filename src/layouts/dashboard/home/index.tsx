e906129cd7a8452e754db74e9efa67d68b7b0b66
Olubodun <bolowys33@gmail.com>
2025-09-19 17:18:05 +0100

    Update

--- src/layouts/dashboard/home/index.tsx
+++ src/layouts/dashboard/home/index.tsx
@@ -119,7 +119,7 @@ const DashboardHome = () => {
           </HStack>
 
           <VStack align="stretch" spaceY={4} mt={6}>
-            <ActiveSavingsCard amount="800,000" name="Rent" plan="6 months" value={90} />
+            <ActiveSavingsCard id="hhh" amount="800,000" name="Rent" plan="6 months" value={90} />
           </VStack>
         </Box>
 
