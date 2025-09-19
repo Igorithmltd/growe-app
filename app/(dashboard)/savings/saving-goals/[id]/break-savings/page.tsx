6eb59646e84dea2c7703f9da059c8dc3dd2a9428
Olubodun <bolowys33@gmail.com>
2025-09-19 16:54:13 +0100

    Update

--- app/(dashboard)/savings/saving-goals/[id]/break-savings/page.tsx
+++ app/(dashboard)/savings/saving-goals/[id]/break-savings/page.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   title: "Growe | Break Savings",
 };
 
-const BreakSavingsPage = async ({ params }: { params: { id: string } }) => {
+const BreakSavingsPage = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
   return <BreakSavingsLayout id={id} />;
 };
