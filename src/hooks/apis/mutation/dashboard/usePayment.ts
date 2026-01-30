d1714844259a612546d91b6e772facb349715c27
Olubodun <bolowys33@gmail.com>
2026-01-30 11:13:28 +0100

    Update

---
+++ src/hooks/apis/mutation/dashboard/usePayment.ts
@@ -0,0 +1,67 @@
+import { AxiosError } from "axios";
+import { useMutation } from "@tanstack/react-query";
+//
+
+import { handleError } from "@/src/utils/helpers";
+import { useFetcher } from "../../../useFetcher";
+import { LoginFormValues } from "@/src/schema/auth.schema";
+import useShowToast from "../../../useShowToast";
+
+type ActionType = "savings" | "investments";
+
+export type PaymentValues = {
+  email: string;
+  amount: string; 
+  actionType: ActionType;
+  actionId: string;
+};
+
+export const usePayment = () => {
+  const showToast = useShowToast();
+
+  const makePayment = async (data: PaymentValues): Promise<AuthResponseData> => {
+    const response = await useFetcher({
+      url: "/utils/initialize-payment",
+      requestType: "POST",
+      body: data,
+      useBaseUrl: true,
+    });
+
+    if (response.error) handleError(response.error);
+
+    return response.data;
+  };
+
+  return useMutation<AuthResponseData, AxiosError<ErrorResponseData>, PaymentValues>({
+    mutationFn: makePayment,
+    onError: (error) => {
+      if (error.response) {
+        const errorData = error.response.data;
+        showToast({
+          title: "Error",
+          description: errorData.message || "Something went wrong on the server",
+          status: "error",
+        });
+      } else if (error.request) {
+        showToast({
+          title: "Network Error",
+          description: "No response received from the server, try again",
+          status: "warning",
+        });
+      } else {
+        showToast({
+          title: "Error",
+          description: error.message || "An unknown error occurred",
+          status: "error",
+        });
+      }
+    },
+    onSuccess: () => {
+    //   showToast({
+    //     title: "Success",
+    //     description: "Logged in successfully.",
+    //     status: "success",
+    //   });
+    },
+  });
+};
