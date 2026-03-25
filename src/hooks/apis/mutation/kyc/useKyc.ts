6cecadc622722f1b1200d9f8d252724acce188a5
Olubodun <bolowys33@gmail.com>
2026-03-25 10:33:10 +0100

    update

--- src/hooks/apis/mutation/kyc/useKyc.ts
+++ src/hooks/apis/mutation/kyc/useKyc.ts
@@ -5,8 +5,11 @@ import { useMutation } from "@tanstack/react-query";
 import { handleError } from "@/src/utils/helpers";
 import { useFetcher } from "../../../useFetcher";
 import { BVNFormValues, NINFormValues } from "@/src/schema/kyc.schema";
+import useShowToast from "@/src/hooks/useShowToast";
 
 export const useKyc = () => {
+  const showToast = useShowToast();
+
   const verifyBvn = async (data: BVNFormValues): Promise<AuthResponseData> => {
     const response = await useFetcher({
       url: "/utils/verify-bvn",
@@ -35,10 +38,68 @@ export const useKyc = () => {
 
   const bvnMutation = useMutation<AuthResponseData, AxiosError<ErrorResponseData>, BVNFormValues>({
     mutationFn: verifyBvn,
+    onError: (error) => {
+      if (error.response) {
+        const errorData = error.response.data;
+        showToast({
+          title: "Error",
+          description: errorData.message || "Failed to send message",
+          status: "error",
+        });
+      } else if (error.request) {
+        showToast({
+          title: "Network Error",
+          description: "No response received from the server, try again",
+          status: "warning",
+        });
+      } else {
+        showToast({
+          title: "Error",
+          description: error.message || "An unknown error occurred",
+          status: "error",
+        });
+      }
+    },
+    onSuccess: () => {
+      showToast({
+        title: "Success",
+        description: "Identity verification successful",
+        status: "success",
+      });
+    },
   });
 
   const ninMutation = useMutation<AuthResponseData, AxiosError<ErrorResponseData>, NINFormValues>({
     mutationFn: verifyNin,
+    onError: (error) => {
+      if (error.response) {
+        const errorData = error.response.data;
+        showToast({
+          title: "Error",
+          description: errorData.message || "Failed to send message",
+          status: "error",
+        });
+      } else if (error.request) {
+        showToast({
+          title: "Network Error",
+          description: "No response received from the server, try again",
+          status: "warning",
+        });
+      } else {
+        showToast({
+          title: "Error",
+          description: error.message || "An unknown error occurred",
+          status: "error",
+        });
+      }
+    },
+    onSuccess: () => {
+      showToast({
+        title: "Success",
+        description: "Identity verification successful",
+        status: "success",
+      });
+    },
   });
 
   return { bvnMutation, ninMutation };
